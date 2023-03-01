db4f1a5edf10de5002c2450cb12846abc8283d78
emre <emreergun@engramsoft.com>
2023-03-01 14:11:20 +0000

    Doxygen related part added

---
+++ 10_UnitTest/src/my_lib/my_lib.cc
@@ -0,0 +1,18 @@
+#include "my_lib.hpp"
+
+#include <nlohmann/json.hpp>
+
+/**
+ * @brief Prints out hello world and tests JSON lib.
+ * 
+ */
+void sayHello(void)
+{
+    std::cout << "Hello World!" << "\n";
+
+    std::cout << "JSON Lib Version: " 
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+}
