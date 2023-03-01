e6674b7bf7d0072fbe69b0bf304978e74d9e4311
emre <emreergun@engramsoft.com>
2023-03-01 15:14:30 +0000

    test part added

--- 10_UnitTest/src/my_lib/my_lib.cc
+++ 10_UnitTest/src/my_lib/my_lib.cc
@@ -1,7 +1,5 @@
 #include "my_lib.hpp"
 
-#include <nlohmann/json.hpp>
-
 /**
  * @brief Prints out hello world and tests JSON lib.
  * 
@@ -9,10 +7,14 @@
 void sayHello(void)
 {
     std::cout << "Hello World!" << "\n";
+}
 
-    std::cout << "JSON Lib Version: " 
-        << NLOHMANN_JSON_VERSION_MAJOR << "."
-        << NLOHMANN_JSON_VERSION_MINOR << "."
-        << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
+/**
+ * @brief Calculates the factorial value of the given argument
+ * 
+ */
+std::uint32_t factorial(std::uint32_t number)
+{
+    return number <= 1 ? 1 : factorial(number - 1) * number;
 }
--- 10_UnitTest/src/my_lib/my_lib.hpp
+++ 10_UnitTest/src/my_lib/my_lib.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <cstdint>
 
 void sayHello(void);
+std::uint32_t factorial(std::uint32_t number);
 
